86b380978101b4ceb16e320678bf63541df121bd
asolace <asolace.jack@gmail.com>
2023-08-20 11:46:45 -0400

    add test, currently not working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,65 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hebrew ELS Search/i);
-  expect(linkElement).toBeInTheDocument();
+describe("ELS React App", () => {
+  test("renders without crashing", () => {
+    render(<App />);
+  });
+
+  test("displays no results when there is no ELS match", async () => {
+    render(<App />);
+    const inputPassage = "אבגדהוזחט";
+    const inputWord = "אז";
+
+    const passageInput = screen.getByPlaceholderText("Enter Hebrew passage");
+    fireEvent.change(passageInput, { target: { value: inputPassage } });
+
+    const intervalInput = screen.getByPlaceholderText("Interval");
+    fireEvent.change(intervalInput, { target: { value: "3" } });
+
+    const wordInput = screen.getByPlaceholderText("Word to match");
+    fireEvent.change(wordInput, { target: { value: inputWord } });
+
+    const searchButton = screen.getByRole("button", { name: "Search" });
+    fireEvent.click(searchButton);
+
+    const noResults = await screen.findByText(
+      "No ELS match found for the provided word."
+    );
+    expect(noResults).toBeInTheDocument();
+  });
+
+  test("displays and highlights letters for a successful ELS match", () => {
+    render(<App />);
+
+    const inputPassage: string = "אבגדהוזחט";
+    const inputWord: string = "אד";
+
+    const passageInput: HTMLElement = screen.getByPlaceholderText(
+      "Enter Hebrew passage"
+    );
+    fireEvent.change(passageInput, { target: { value: inputPassage } });
+
+    const intervalInput: HTMLElement = screen.getByPlaceholderText("Interval");
+    fireEvent.change(intervalInput, { target: { value: "3" } });
+
+    const wordInput: HTMLElement = screen.getByPlaceholderText("Word to match");
+    fireEvent.change(wordInput, { target: { value: inputWord } });
+
+    const searchButton: HTMLElement = screen.getByRole("button", {
+      name: "Search",
+    });
+    fireEvent.click(searchButton);
+
+    // Check if the passage contains the highlighted word
+    const highlightedLetters: NodeListOf<HTMLElement> = screen
+      .getByText(inputPassage)
+      .querySelectorAll('span[style="background-color: yellow;"]');
+    const highlightedWord: string = Array.from(highlightedLetters)
+      .map((span) => span.textContent)
+      .join("");
+
+    expect(highlightedWord).toBe(inputWord);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -9,98 +9,160 @@ import { Container, Grid } from "@mui/joy";
 import { Box } from "@mui/material";
 
 function App() {
-  const [hebrewPassage, setHebrewPassage] = useState<string>("");
-  const [skipIntervals, setSkipIntervals] = useState<number>(1);
-  const [hebrewWord, setHebrewWord] = useState<string[]>([]);
-  const [startWord, setStartWord] = useState<string>(""); // New state for starting Hebrew word
-  const [result, setResult] = useState<string>("");
+  // const [hebrewPassage, setHebrewPassage] = useState<string>("");
+  // const [skipIntervals, setSkipIntervals] = useState<number>(1);
+  // const [hebrewWord, setHebrewWord] = useState<string[]>([]);
+  // const [startWord, setStartWord] = useState<string>(""); // New state for starting Hebrew word
+  // const [result, setResult] = useState<string>("");
 
-  const handleHebrewPassageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setHebrewPassage(e.target.value);
+  // const handleHebrewPassageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  //   setHebrewPassage(e.target.value);
+  // };
+
+  // const handleSkipIntervalsChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setSkipIntervals(parseInt(e.target.value));
+  // };
+
+  // const handleStartWordChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setStartWord(e.target.value);
+  // };
+
+  // const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const elsResult = FindELS(hebrewPassage, skipIntervals, startWord);
+  //   setResult(elsResult);
+  // };
+
+  // return (
+  //   <div className="App">
+  //     <Container
+  //       maxWidth="xl"
+  //       sx={{
+  //         margin: "auto",
+  //       }}
+  //     >
+  //       <Typography
+  //         level="h1"
+  //         sx={{
+  //           marginBottom: 2,
+  //         }}
+  //       >
+  //         Hebrew ELS Search
+  //       </Typography>
+
+  //       <Grid container spacing={8}>
+  //         <Grid xs={6}>
+  //           <form onSubmit={handleSubmit}>
+  //             <Grid container spacing={8}>
+  //               <Grid xs={12}>
+  //                 <InputField
+  //                   label="Starting Word"
+  //                   placeholder="Starting Word"
+  //                   helperText="The word you want to start the search from"
+  //                   value={startWord}
+  //                   onChange={handleStartWordChange}
+  //                 />
+  //               </Grid>
+  //               <Grid xs={12}>
+  //                 <InputField
+  //                   label="Skip Intervals"
+  //                   placeholder="Skip Intervals"
+  //                   helperText="The number of letters to skip between each letter in the sequence"
+  //                   value={skipIntervals}
+  //                   onChange={handleSkipIntervalsChange}
+  //                 />
+  //               </Grid>
+  //               <Grid xs={12}>
+  //                 <TextareaField
+  //                   label="Hebrew Passage"
+  //                   placeholder="Hebrew Passage"
+  //                   helperText="The hebrew passage you want to search in"
+  //                   value={hebrewPassage}
+  //                   onChange={handleHebrewPassageChange}
+  //                   minRows={10}
+  //                 />
+  //               </Grid>
+  //               <Grid xs={12}>
+  //                 <Button type="submit">Submit</Button>
+  //               </Grid>
+  //             </Grid>
+  //           </form>
+  //         </Grid>
+  //         <Grid xs={6}>
+  //           <Typography
+  //             level="h3"
+  //             sx={{
+  //               marginTop: 2,
+  //             }}
+  //           >
+  //             Result
+  //           </Typography>
+  //           <Typography>{result}</Typography>
+  //         </Grid>
+  //       </Grid>
+  //     </Container>
+  //   </div>
+  // );
+
+  // ChatGPT https://chat.openai.com/c/2df8c408-378f-46bd-95d8-5436673f7d1f
+  const [passage, setPassage] = useState<string>("");
+  const [interval, setInterval] = useState<number>(1);
+  const [word, setWord] = useState<string>("");
+  const [highlightedIndices, setHighlightedIndices] = useState<number[]>([]);
+
+  const filterHebrew = (text: string) => {
+    return text.replace(/[^\u0590-\u05FF]+/g, "");
   };
 
-  const handleSkipIntervalsChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSkipIntervals(parseInt(e.target.value));
-  };
+  const findELS = () => {
+    const hebrewPassage = filterHebrew(passage);
+    let sequence = "";
+    let indices: number[] = [];
 
-  const handleStartWordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setStartWord(e.target.value);
-  };
+    for (let i = 0; i < hebrewPassage.length; i += interval) {
+      sequence += hebrewPassage[i];
+      indices.push(passage.indexOf(hebrewPassage[i]));
+    }
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const elsResult = FindELS(hebrewPassage, skipIntervals, startWord);
-    setResult(elsResult);
+    setHighlightedIndices(sequence.includes(word) ? indices : []);
   };
 
   return (
-    <div className="App">
-      <Container
-        maxWidth="xl"
-        sx={{
-          margin: "auto",
-        }}
-      >
-        <Typography
-          level="h1"
-          sx={{
-            marginBottom: 2,
-          }}
-        >
-          Hebrew ELS Search
-        </Typography>
-
-        <Grid container spacing={8}>
-          <Grid xs={6}>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={8}>
-                <Grid xs={12}>
-                  <InputField
-                    label="Starting Word"
-                    placeholder="Starting Word"
-                    helperText="The word you want to start the search from"
-                    value={startWord}
-                    onChange={handleStartWordChange}
-                  />
-                </Grid>
-                <Grid xs={12}>
-                  <InputField
-                    label="Skip Intervals"
-                    placeholder="Skip Intervals"
-                    helperText="The number of letters to skip between each letter in the sequence"
-                    value={skipIntervals}
-                    onChange={handleSkipIntervalsChange}
-                  />
-                </Grid>
-                <Grid xs={12}>
-                  <TextareaField
-                    label="Hebrew Passage"
-                    placeholder="Hebrew Passage"
-                    helperText="The hebrew passage you want to search in"
-                    value={hebrewPassage}
-                    onChange={handleHebrewPassageChange}
-                    minRows={10}
-                  />
-                </Grid>
-                <Grid xs={12}>
-                  <Button type="submit">Submit</Button>
-                </Grid>
-              </Grid>
-            </form>
-          </Grid>
-          <Grid xs={6}>
-            <Typography
-              level="h3"
-              sx={{
-                marginTop: 2,
-              }}
-            >
-              Result
-            </Typography>
-            <Typography>{result}</Typography>
-          </Grid>
-        </Grid>
-      </Container>
+    <div>
+      <h1>ELS React App</h1>
+      <textarea
+        value={passage}
+        onChange={(e) => setPassage(e.target.value)}
+        placeholder="Enter Hebrew passage"
+      />
+      <input
+        type="number"
+        value={interval}
+        onChange={(e) => setInterval(Number(e.target.value))}
+        placeholder="Interval"
+      />
+      <input
+        type="text"
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
+        placeholder="Word to match"
+      />
+      <button onClick={findELS}>Search</button>
+      <h2>Passage:</h2>
+      <p>
+        {passage.split("").map((char, index) => (
+          <span
+            key={index}
+            style={
+              highlightedIndices.includes(index)
+                ? { backgroundColor: "yellow" }
+                : {}
+            }
+          >
+            {char}
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
